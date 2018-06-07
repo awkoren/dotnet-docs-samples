af617ed74af0afa922f2a1c408b3845b0700b631
Alexander Koren <awkoren@google.com>
2018-06-07 18:10:55 -0400

    Continue building service account code; format according to other samples

--- iam/api/ServiceAccounts/Program.cs
+++ iam/api/ServiceAccounts/Program.cs
@@ -14,24 +14,155 @@
 
 using System;
 using System.Collections.Generic;
+using CommandLine;
 using Google.Apis.Auth.OAuth2;
 using Google.Apis.Iam.v1;
 using Google.Apis.Iam.v1.Data;
 
-public static class ServiceAccounts
+namespace GoogleCloudSamples
 {
-    private static GoogleCredential credential; 
-    private static IamService iam;
-    private static string projectId;
-
-    public static void Main(string[] args)
+    [Verb("service-accounts-create", HelpText = "Creates a service account.")]
+    internal class CreateServiceAccountOptions
     {
-        credential = GoogleCredential.GetApplicationDefault()
-            .CreateScoped(IamService.Scope.CloudPlatform);
-        iam = new IamService(new IamService.Initializer
+        [Option("project-id", HelpText = "The project Id.", Required = true)]
+        public string ProjectId { get; set; }
+
+        [Option("name", HelpText = "The service account's name.", Required = true)]
+        public string Name { get; set; }
+
+        [Option("display-name", HelpText = "The service account's friendly display name.")]
+        public string DisplayName { get; set; }
+    }
+
+    [Verb("service-accounts-list", HelpText = "Lists service accounts.")]
+    internal class ListServiceAccountOptions
+    {
+        [Option("project-id", HelpText = "The project Id.", Required = true)]
+        public string ProjectId { get; set; }
+    }
+
+    [Verb("service-accounts-rename", HelpText = "Updates a service account's display name.")]
+    internal class RenameServiceAccountOptions
+    {
+        [Option("project-id", HelpText = "The project Id.", Required = true)]
+        public string ProjectId { get; set; }
+
+        [Option("name", HelpText = "The service account name.", Required = true)]
+        public string Name { get; set; }
+
+        [Option("name", HelpText = "The servie account's new friendly display name.", Required = true)]
+        public string DisplayName { get; set; }
+    }
+
+    [Verb("service-accounts-delete", HelpText = "Deletes a service account.")]
+    internal class DeleteServiceAccountOptions
+    {
+        [Option("project-id", HelpText = "The project Id.", Required = true)]
+        public string ProjectId { get; set; }
+
+        [Option("name", HelpText = "The service account's name.", Required = true)]
+        public string Name { get; set; }
+    }
+
+    public static class Program
+    {
+        private static IamService iam;
+
+        public static int CreateServiceAccount(string projectId, string name,
+            string displayName)
         {
-            HttpClientInitializer = credential
-        });
-        projectId = "projects/awkoren-iam";
+            // [START iam_create_service_account]
+            var request = new CreateServiceAccountRequest
+            {
+                AccountId = name,
+                ServiceAccount = new ServiceAccount
+                {
+                    DisplayName = displayName
+                }
+            };
+
+            ServiceAccount serviceAccount = iam.Projects.ServiceAccounts
+                .Create(request, "projects/" + projectId).Execute();
+
+            Console.WriteLine("Created service account: " + serviceAccount.Email);
+            // [END iam_create_service_account]
+            return 0;
+        }
+
+        public static int ListServiceAccounts(string projectId)
+        {
+            // [START iam_list_service_accounts]
+            ListServiceAccountsResponse response = iam.Projects.ServiceAccounts
+                .List("projects/" + projectId).Execute();
+            IList<ServiceAccount> serviceAccounts = response.Accounts;
+
+            foreach (ServiceAccount account in serviceAccounts)
+            {
+                Console.WriteLine("Name: " + account.Name);
+                Console.WriteLine("Display Name: " + account.DisplayName);
+                Console.WriteLine("Email: " + account.Email);
+                Console.WriteLine();
+            }
+            // [END iam_list_service_accounts]
+            return 0;
+        }
+
+        public static int RenameServiceAccount(string projectId, string name,
+            string newDisplayName)
+        {
+            // [START iam_rename_service_account]
+            // First, get a ServiceAccount using List() Get()
+            string email = $"{name}@{projectId}.iam.gserviceaccount.com";
+            string resource = $@"projects/{projectId}/serviceAccounts/{email}";
+            ServiceAccount serviceAccount = iam.Projects.ServiceAccounts
+                .Get(resource).Execute();
+
+            // Then you can update the display name
+            serviceAccount.DisplayName = newDisplayName;
+            serviceAccount = iam.Projects.ServiceAccounts.Update(
+                serviceAccount, resource).Execute();
+
+            Console.WriteLine($"Updated display name for {serviceAccount.Email} " +
+                "to: " + serviceAccount.DisplayName);
+            // [END iam_rename_service_account]
+            return 0;
+        }
+
+        public static int DeleteServiceAccount(string projectId, string email)
+        {
+            // [START iam_delete_service_account]
+            string resource = $@"projects/{projectId}/serviceAccounts/{email}";
+            iam.Projects.ServiceAccounts.Delete(resource).Execute();
+
+            Console.WriteLine("Deleted service account: " + email);
+            // [END iam_delete_service_account]
+            return 0;
+        }
+
+        public static void Main(string[] args)
+        {
+            GoogleCredential credential = GoogleCredential.GetApplicationDefault()
+                .CreateScoped(IamService.Scope.CloudPlatform);
+            iam = new IamService(new IamService.Initializer
+            {
+                HttpClientInitializer = credential
+            });
+
+            Parser.Default.ParseArguments<
+                CreateServiceAccountOptions,
+                ListServiceAccountOptions,
+                RenameServiceAccountOptions,
+                DeleteServiceAccountOptions
+                >(args).MapResult(
+                (CreateServiceAccountOptions x) => CreateServiceAccount(
+                    x.ProjectId, x.Name, x.DisplayName),
+                (ListServiceAccountOptions x) => ListServiceAccounts(
+                    x.ProjectId),
+                (RenameServiceAccountOptions x) => RenameServiceAccount(
+                    x.ProjectId, x.Name, x.DisplayName),
+                (DeleteServiceAccountOptions x) => DeleteServiceAccount(
+                    x.ProjectId, x.Name),
+                error => 1);
+        }
     }
 }
